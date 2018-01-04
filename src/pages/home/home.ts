c4aa003ed55fa67aa30e77499a0dbb6cf4d4ca74
Jordão Macedo <jordao05@hotmail.com>
2018-01-04 19:21:05 -0300

    Fix scraping for third table

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -16,6 +16,7 @@ export class HomePage {
     public isRefreshing: boolean = false;
 
     public items1: Array<any> = [];
+    public items3: Array<any> = []; // Items da tabela 3
 
     constructor(
         public navCtrl: NavController,
@@ -51,6 +52,7 @@ export class HomePage {
                 let tableSplit = this.data.split("<table class=\"w728\">");
                 this.workInTable(tableSplit[1]); //1º tebela
                 this.workInTable(tableSplit[2]); //2º tebela
+                this.workIn3Table(tableSplit[3]); //3º tebela
                 //this.workInTable(tableSplit[4]); //4º tebela
 
                 this.fecharCarregandoHome();
@@ -79,6 +81,12 @@ export class HomePage {
         this.getItemRecursive(arrItems);
     }
 
+    workIn3Table(table: string){
+        var arrItems = table.split("<span class=\"txt\">");
+        //arrItems.shift();
+        this.getItemRecursive3(arrItems);
+    }
+
     getItemRecursive(arrayHtml: Array<string>) {
         if (arrayHtml.length <= 0)
             return;
@@ -92,10 +100,52 @@ export class HomePage {
             "value" : value
         }
         this.items1.push(item);
-        console.log(key + ": " + value);
+        //console.log(key + ": " + value);
 
         arrayHtml.shift();
         arrayHtml.shift();
         this.getItemRecursive(arrayHtml);
+    }
+
+    getItemRecursive3(arrayHtml: Array<string>) {
+        if (arrayHtml.length <= 0)
+            return;
+        let begin = arrayHtml[0];
+        if(begin.indexOf("class=\"nivel1\"") !== -1){
+            arrayHtml.shift();
+            this.getItemRecursive3(arrayHtml);
+            return;
+        }
+        arrayHtml.shift();
+
+        let itemKeyHtml = arrayHtml[0];
+        if(!itemKeyHtml){ // Se for um TD em branco
+            this.getItemRecursive3(arrayHtml);
+            return;
+        }
+        let key = itemKeyHtml.split("</span>")[0];
+        if(key == ""){ // Se a key for vazia
+            this.getItemRecursive3(arrayHtml);
+            return;
+        }
+        let value;
+
+        if(itemKeyHtml.indexOf("<span class=\"oscil\">") !== -1){
+            let spanSplit = itemKeyHtml.split("<span class=\"oscil\">")[1];
+            let fontSplit = spanSplit.split(">")[1];
+            value = fontSplit.split("</font")[0];
+        } else {
+            let itemValueHtml = arrayHtml[1];
+            value = itemValueHtml.split("</span>")[0];
+            arrayHtml.shift();
+        }
+        
+        let item = {
+            "key": key,
+            "value" : value
+        }
+        this.items3.push(item);
+
+        this.getItemRecursive3(arrayHtml);
     }
 }
